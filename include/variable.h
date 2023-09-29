4e3aabcaef20358a3a9a73d25179e48774b0bc0b
a3510377 <a102009102009@gmail.com>
2023-09-29 22:42:56 +0800

    setup

---
+++ include/variable.h
@@ -0,0 +1,12 @@
+/*
+  - id: SSD1306 OLED
+    address: 0x3C
+*/
+#define I2C_SDA 22  // I2C SDA
+#define I2C_SCL 23  // I2C SCL
+
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 64  // OLED display height, in pixels
+
+#define PMS7003_RX 22
+#define PMS7003_TX 22
