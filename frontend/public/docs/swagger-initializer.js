3cec31731c4f3997fd13f94c8357efbd4fb62242
a3510377 <a102009102009@gmail.com>
2023-10-12 06:15:34 +0800

    Update swagger-initializer.js

--- frontend/public/docs/swagger-initializer.js
+++ frontend/public/docs/swagger-initializer.js
@@ -1,6 +1,6 @@
 window.onload = async () => {
   window.ui = SwaggerUIBundle({
-    url: 'swagger.json',
+    url: 'https://raw.githubusercontent.com/a3510377/Elder-Care-App/assets/swagger.json',
     dom_id: '#swagger-ui',
     deepLinking: !0,
     presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
