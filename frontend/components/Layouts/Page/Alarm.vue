262ab8433a08256e9a1a9fb1f426591feb0a8795
a3510377 <a102009102009@gmail.com>
2023-10-13 19:47:59 +0800

    Create Alarm.vue

---
+++ frontend/components/Layouts/Page/Alarm.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="fixed bottom-8 right-8 gap-3 flex flex-col">
+    <div
+      class="relative w-56 h-20 bg-slate-100 rounded-lg transition-transform -translate-y-5 duration-300"
+      v-for="_ in 2"
+    >
+      <div
+        class="top-2 right-2 absolute rounded-full p-0.5 cursor-pointer hover:bg-slate-300"
+        @click="() => console.log('awa')"
+      >
+        <Icon name="mdi:close" class="block" width="18px" height="18px" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped></style>
