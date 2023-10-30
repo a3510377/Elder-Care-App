d86c016feb0d19b6a90c85695859137f3fd0cfda
a3510377 <a102009102009@gmail.com>
2023-10-30 17:40:49 +0800

    Update index.vue

--- frontend/pages/users/index.vue
+++ frontend/pages/users/index.vue
@@ -4,7 +4,12 @@
     <div
       class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 p-4 text-gray-50"
     >
-      <PageUser v-for="(user, i) in data?.body" :key="i" :user="user" />
+      <PageUser
+        :class="{ 'animate-warn': alarmMapUserIds.includes(user.id) }"
+        v-for="(user, i) in data?.body"
+        :key="i"
+        :user="user"
+      />
     </div>
   </div>
 </template>
@@ -16,8 +21,24 @@ const {
   public: { apiUrl },
 } = useRuntimeConfig();
 
+const alarmMap = useAlarmMap();
+const alarmMapUserIds = computed(() => alarmMap.map((d) => d.id));
+
 const { data: data } = await useFetch<APIRequestBody<User[]>>(
   `${apiUrl}/api/users`,
   { server: false }
 );
 </script>
+
+<style lang="scss" scoped>
+.animate-warn {
+  animation: flash 500ms;
+  animation-iteration-count: infinite;
+  transition: border-color 500ms ease-out;
+}
+@keyframes flash {
+  to {
+    border-color: red;
+  }
+}
+</style>
